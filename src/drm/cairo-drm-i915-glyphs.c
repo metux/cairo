1a8f7ad9c515c630152f8e7fa04062274d0a36b9
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2015-12-09 08:57:25 +0100

    drm/i915: fixed prototype of surface_backend => show_glyphs() operations
    These operations dont have the num_remaining paramter anymore,
    the DRM backend(s) haven't been updated yet.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>

--- src/drm/cairo-drm-i915-glyphs.c
+++ src/drm/cairo-drm-i915-glyphs.c
@@ -279,8 +279,7 @@ i915_surface_glyphs (void			*abstract_surface,
 		     cairo_glyph_t		*glyphs,
 		     int			 num_glyphs,
 		     cairo_scaled_font_t	*scaled_font,
-		     const cairo_clip_t		*clip,
-		     int *num_remaining)
+		     const cairo_clip_t		*clip)
 {
     i915_surface_t *surface = abstract_surface;
     i915_surface_t *mask = NULL;
@@ -298,7 +297,6 @@ i915_surface_glyphs (void			*abstract_surface,
     int mask_x = 0, mask_y = 0;
     int i = 0;
 
-    *num_remaining = 0;
     status = _cairo_composite_rectangles_init_for_glyphs (&extents,
 							  surface->intel.drm.width,
 							  surface->intel.drm.height,
