1ce8f700d4026160b38b08c91267819a73d196f6
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2015-12-10 22:03:43 +0100

    drm: allow CAIRO_FORMAT_RGB30
    Signed-off-by: Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>

--- src/drm/cairo-drm-i965-surface.c
+++ src/drm/cairo-drm-i965-surface.c
@@ -1615,6 +1615,7 @@ i965_surface_create (cairo_drm_device_t *device,
     case CAIRO_FORMAT_ARGB32:
     case CAIRO_FORMAT_RGB16_565:
     case CAIRO_FORMAT_RGB24:
+    case CAIRO_FORMAT_RGB30:
     case CAIRO_FORMAT_A8:
 	break;
     case CAIRO_FORMAT_INVALID:
@@ -1649,6 +1650,7 @@ i965_surface_create_for_name (cairo_drm_device_t *base_dev,
     case CAIRO_FORMAT_ARGB32:
     case CAIRO_FORMAT_RGB16_565:
     case CAIRO_FORMAT_RGB24:
+    case CAIRO_FORMAT_RGB30:
     case CAIRO_FORMAT_A8:
 	break;
     case CAIRO_FORMAT_INVALID:
