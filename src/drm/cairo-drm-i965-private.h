3cffc3fbea94a7fd3f64337e19c18a54a8188749
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2015-12-10 22:03:05 +0100

    drm: fixed prototype of surface_backend => show_glyphs() operations
    These operations dont have the num_remaining paramter anymore,
    the DRM backend(s) haven't been updated yet.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>

--- src/drm/cairo-drm-i965-private.h
+++ src/drm/cairo-drm-i965-private.h
@@ -695,8 +695,7 @@ i965_surface_glyphs (void			*abstract_surface,
 		     cairo_glyph_t		*glyphs,
 		     int			 num_glyphs,
 		     cairo_scaled_font_t	*scaled_font,
-		     const cairo_clip_t		*clip,
-		     int *num_remaining);
+		     const cairo_clip_t		*clip);
 
 cairo_private void
 i965_shader_init (i965_shader_t *shader,
